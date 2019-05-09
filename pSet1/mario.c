0b453691cd383926ad14ca523b4a8379bc632d36
dl769 <49956347+dl769@users.noreply.github.com>
2019-05-09 23:29:16 +0200

    Update mario.c

--- pSet1/mario.c
+++ pSet1/mario.c
@@ -9,29 +9,36 @@ int main(void)
 {
     int height;
     
-    do{
+    do
+    {
 
     printf("Height: ");
     scanf("%d",&height);
         
-    }while(check(height)!=1);
+    }
+    while(check(height)!=1);
 
     createpyramids(height);
 }
 
-int check(int height){
+int check(int height)
+{
 
-    if(height>=1&&height<=8){
+    if(height>=1&&height<=8)
+    {
 
     return 1;
 
-    }else return 0;
+    }
+    else return 0;
 
  }
 
-int createpyramids(int x){
+int createpyramids(int x)
+{
 
-    for(int i=1;i<=x;i++){
+    for(int i=1;i<=x;i++)
+    {
 
        for(int a=x;a>i;a--) printf(" ");
        for(int a=1;a<i+1;a++) printf("#");
